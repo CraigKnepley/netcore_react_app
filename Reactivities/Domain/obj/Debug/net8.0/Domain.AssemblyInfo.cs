033083aca5ea17157aa5d5fbc6c3968f4dbf1e08
CraigKnepley <116113213+CraigKnepley@users.noreply.github.com>
2025-01-14 19:30:50 +0000

    Adding Activity class

--- Reactivities/Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
+++ Reactivities/Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8fcf429347fa098b6a6ad37b728a4e032b58c584")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+419a93273f0fe937e3c2570d2de4d3ab201b2020")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
